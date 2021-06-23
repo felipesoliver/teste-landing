33419b2ed92bde2fede0929efc3fc87070ba3625
Felipe Oliveira <felipephito@gmail.com>
2021-06-23 12:17:43 -0300

    Implemented script's file with modules

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+import initScrollAnimation from './scroll-animation.js';
+import initBurgerBtn from './burger-btn.js'
+import initToggleNavBar from './toggle-menu.js';
+
+initScrollAnimation();
+initBurgerBtn();
+initToggleNavBar();
