c3cef2574aef3ddb028537a46a7f3945073bc720
Felipe Oliveira <felipephito@gmail.com>
2021-06-23 12:18:09 -0300

    Added animation on scroll on project

---
+++ src/js/scroll-animation.js
@@ -0,0 +1,20 @@
+export default function initScrollAnimation() {
+  const sections = document.querySelectorAll('[data-aos]');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function AOS() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if (isSectionVisible) {
+        section.classList.add('animate');
+      } else if (section.classList.contains('animate')) {
+        section.classList.remove('animate');
+      }
+    });
+  }
+  if (sections.length) {
+    AOS();
+    window.addEventListener('scroll', AOS);
+  }
+}
